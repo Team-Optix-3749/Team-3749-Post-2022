a2c06187ac0d23e8b7811d79a707f84933b26d7e
rohin <rs.rohinsood@gmail.com>
2022-08-06 14:57:06 -0700

    remove neo specific paramter

--- Team-3749-Post-2022/src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ Team-3749-Post-2022/src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -3,15 +3,14 @@ package frc.robot.subsystems.elevator;
 import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.elevator.ElevatorIONeo;
 import frc.robot.subsystems.elevator.ElevatorIO.ElevatorIOInputs;
 
 public class Elevator extends SubsystemBase {
 
-  private final ElevatorIONeo io;
+  private final ElevatorIO io;
   private final ElevatorIOInputs inputs = new ElevatorIOInputs();
 
-  public Elevator(ElevatorIONeo io) {
+  public Elevator(ElevatorIO io) {
     this.io = io;
 
     io.setBrakeMode(true);
