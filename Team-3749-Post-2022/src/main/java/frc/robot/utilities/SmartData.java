d5cd1fbe9d3d7d418507e88740fd2fd027770d9c
rohin <rs.rohinsood@gmail.com>
2022-08-07 13:46:03 -0700

    add setdefault method to smartdata

---
+++ Team-3749-Post-2022/src/main/java/frc/robot/utilities/SmartData.java
@@ -0,0 +1,39 @@
+package frc.robot.utilities;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTableType;
+
+/***
+ * @author Rohan Juneja
+ * 
+ * Stores data that can be seen/edited in Smart Dashboard
+ * @param <T> Type of data: Supported types include String, Double, and Boolean
+ */
+public class SmartData<T> {
+    T defaultVal;
+    NetworkTableEntry entry;
+
+    public SmartData (String name, T defaultVal) {
+        this.defaultVal = defaultVal;
+        entry = NetworkTableInstance.getDefault().getTable("SmartDashboard").getEntry(name);
+        entry.setValue(defaultVal);
+    }
+
+    public void setDefault (T defaultVal) {
+        this.defaultVal = defaultVal;
+    }
+
+    @SuppressWarnings("unchecked") 
+    public T get() {
+        Object value = entry.getValue();
+        if (value == NetworkTableType.kUnassigned) {
+            return defaultVal;
+        } 
+        return (T) value;
+    }
+    
+    public void set(T val) {
+        entry.setValue(val);
+    }
+}
