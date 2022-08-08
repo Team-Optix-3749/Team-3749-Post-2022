9e6b4280b8cdf23abbb765d0cf5773d9be4cebde
rohin <rs.rohinsood@gmail.com>
2022-08-07 17:00:28 -0700

    fix indents + class declaration

--- Team-3749-Post-2022/src/main/java/frc/robot/utilities/SmartArray.java
+++ Team-3749-Post-2022/src/main/java/frc/robot/utilities/SmartArray.java
@@ -9,46 +9,47 @@ import edu.wpi.first.networktables.NetworkTableType;
 /***
  * @author Rohan Juneja
  * 
- * Stores data array that can be seen/edited in Smart Dashboard
+ *         Stores data array that can be seen/edited in Smart Dashboard
  * @param <T> Type of data: Supported types include String, Double, and Boolean
  */
 public class SmartArray<T> {
   ArrayList<T> defaultVal = new ArrayList<T>();
   NetworkTableEntry entry;
 
-  public SmartArray (String name) {
-      entry = NetworkTableInstance.getDefault().getTable("SmartDashboard").getEntry(name);
-      entry.setValue(defaultVal.toArray());
+  public SmartArray(String name) {
+    defaultVal = new ArrayList<T>();
+    entry = NetworkTableInstance.getDefault().getTable("SmartDashboard").getEntry(name);
+    entry.setValue(defaultVal.toArray());
   }
 
-  public SmartArray (String name, ArrayList<T> defaultVal) {
+  public SmartArray(String name, ArrayList<T> defaultVal) {
     this.defaultVal = defaultVal;
     entry = NetworkTableInstance.getDefault().getTable("SmartDashboard").getEntry(name);
     entry.setValue(defaultVal.toArray());
   }
 
-  public void setDefault (ArrayList<T> defaultVal) {
-      this.defaultVal = defaultVal;
+  public void setDefault(ArrayList<T> defaultVal) {
+    this.defaultVal = defaultVal;
   }
 
-  @SuppressWarnings("unchecked") 
+  @SuppressWarnings("unchecked")
   public ArrayList<T> get() {
-      Object value = entry.getValue();
-      if (value == NetworkTableType.kUnassigned) {
-          return defaultVal;
-      } 
-      return (ArrayList<T>) value;
+    Object value = entry.getValue();
+    if (value == NetworkTableType.kUnassigned) {
+      return defaultVal;
+    }
+    return (ArrayList<T>) value;
   }
 
   public void set(T[] val) {
-      entry.setValue(val);
+    entry.setValue(val);
   }
 
   public void addValue(T val) {
-      ArrayList<T> current = get();
+    ArrayList<T> current = get();
 
-      current.add(val);
+    current.add(val);
 
-      entry.setValue(current.toArray());
+    entry.setValue(current.toArray());
   }
 }
