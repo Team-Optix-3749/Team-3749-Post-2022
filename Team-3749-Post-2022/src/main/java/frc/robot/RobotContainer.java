017642d5128b148d476cde8256d3c4bc81c0f79f
rohin <rs.rohinsood@gmail.com>
2022-08-07 17:14:51 -0700

    robotcontainer setup for drivetrain

--- Team-3749-Post-2022/src/main/java/frc/robot/RobotContainer.java
+++ Team-3749-Post-2022/src/main/java/frc/robot/RobotContainer.java
@@ -5,32 +5,31 @@
 package frc.robot;
 
 import frc.robot.utilities.*;
-import frc.robot.commands.LowerElevator;
-import frc.robot.commands.RaiseElevator;
-import frc.robot.commands.StopElevator;
+import frc.robot.commands.drivetrain.TankDrive;
+import frc.robot.commands.elevator.*;
 import frc.robot.oi.HandheldOI;
 import frc.robot.oi.OISelector;
-import frc.robot.oi.OverrideOI;
+import frc.robot.subsystems.drivetrain.Drivetrain;
+import frc.robot.subsystems.drivetrain.DrivetrainIOTalonFX;
 import frc.robot.subsystems.elevator.*;
 
-import com.fasterxml.jackson.databind.ser.std.NumberSerializers.IntegerSerializer;
-
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class RobotContainer {
 
   private final Elevator m_elevator;
+
+  private final Drivetrain m_drivetrain;
 
   private HandheldOI handheldOI;
 
   XboxController m_pilot = new XboxController(0);
 
   public RobotContainer() {
+
+    m_drivetrain = new Drivetrain(new DrivetrainIOTalonFX() {});
 
     m_elevator = new Elevator(new ElevatorIONeo() {});
 
@@ -40,23 +39,21 @@ public class RobotContainer {
   public void updateOI() {
     CommandScheduler.getInstance().clearButtons();
 
-    // JoystickButton A = new JoystickButton(m_pilot, Button.kA.value);
-    // JoystickButton B = new JoystickButton(m_pilot, Button.kB.value);
-
-    // A.whenHeld(new RaiseElevator(m_elevator))
-    //   .whenReleased(new StopElevator(m_elevator));
-
-    // B.whenHeld(new LowerElevator(m_elevator))
-    //   .whenReleased(new StopElevator(m_elevator));
-
     handheldOI = OISelector.findHandheldOI();
 
     handheldOI.getRaiseElevatorButton()
-      .whenHeld(new RaiseElevator(m_elevator));
+      .whenHeld(new RaiseElevator(m_elevator))
+      .whenReleased(new StopElevator(m_elevator));
       
     handheldOI.getLowerElevatorButton()
-      .whenHeld(new LowerElevator(m_elevator));
+      .whenHeld(new LowerElevator(m_elevator))
+      .whenReleased(new StopElevator(m_elevator));
+    
 
+    m_drivetrain.setDefaultCommand(
+      new TankDrive(
+        m_drivetrain, handheldOI::getLeftX, handheldOI::getLeftY,
+        handheldOI::getRightX, handheldOI::getRightY));
   }
 
   public Command getAutonomousCommand() {
