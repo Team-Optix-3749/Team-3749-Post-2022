aa1d04821a9d5af3dcedde7a92a5400ab2ccb48c
Dash Penning <dashpenning@gmail.com>
2022-12-07 16:08:46 -0800

    tested smartdata & smartarray

---
+++ Team-3749-Post-2022/src/main/java/frc/robot/utilities/SmartArray.java
@@ -0,0 +1,55 @@
+package frc.robot.utilities;
+
+import java.util.ArrayList;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTableType;
+
+/***
+ * @author Rohan Juneja
+ * 
+ *         Stores data array that can be seen/edited in Smart Dashboard
+ * @param <T> Type of data: Supported types include String, Double, and Boolean
+ */
+public class SmartArray<T> {
+  ArrayList<T> defaultVal = new ArrayList<T>();
+  NetworkTableEntry entry;
+
+  public SmartArray(String name) {
+    defaultVal = new ArrayList<T>();
+    entry = NetworkTableInstance.getDefault().getTable("SmartDashboard").getEntry(name);
+    entry.setValue(defaultVal.toArray());
+  }
+
+  public SmartArray(String name, ArrayList<T> defaultVal) {
+    this.defaultVal = defaultVal;
+    entry = NetworkTableInstance.getDefault().getTable("SmartDashboard").getEntry(name);
+    entry.setValue(defaultVal.toArray());
+  }
+
+  public void setDefault(ArrayList<T> defaultVal) {
+    this.defaultVal = defaultVal;
+  }
+
+  @SuppressWarnings("unchecked")
+  public ArrayList<T> get() {
+    Object value = entry.getValue();
+    if (value == NetworkTableType.kUnassigned) {
+      return defaultVal;
+    }
+    return (ArrayList<T>) value;
+  }
+
+  public void set(T[] val) {
+    entry.setValue(val);
+  }
+
+  public void addValue(T val) {
+    ArrayList<T> current = get();
+
+    current.add(val);
+
+    entry.setValue(current.toArray());
+  }
+}
--- Team-3749-Post-2022/src/main/java/frc/robot/utilities/SmartData.java
+++ Team-3749-Post-2022/src/main/java/frc/robot/utilities/SmartData.java
@@ -3,9 +3,11 @@ package frc.robot.utilities;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.NetworkTableType;
+import edu.wpi.first.networktables.NetworkTableValue;
 
 /***
  * @author Rohan Juneja
+ * 
  * Stores data that can be seen/edited in Smart Dashboard
  * @param <T> Type of data: Supported types include String, Double, and Boolean
  */
@@ -19,13 +21,26 @@ public class SmartData<T> {
         entry.setValue(defaultVal);
     }
 
+    public void setDefault (T defaultVal) {
+        this.defaultVal = defaultVal;
+    }
+
     @SuppressWarnings("unchecked") 
     public T get() {
-        Object value = entry.getValue();
-        if (value == NetworkTableType.kUnassigned) {
-            return defaultVal;
-        } 
-        return (T) value;
+        NetworkTableValue value = entry.getValue();
+
+        if (value.getType() == NetworkTableType.kBoolean) {
+            return  (T) (Boolean) value.getBoolean();
+
+        } else if (value.getType() == NetworkTableType.kDouble) {
+            return  (T) (Double) value.getDouble();
+
+        } else if (value.getType() == NetworkTableType.kString) {
+            return  (T) (String) value.getString();
+            
+        } else {
+            return this.defaultVal;
+        }
     }
     
     public void set(T val) {
